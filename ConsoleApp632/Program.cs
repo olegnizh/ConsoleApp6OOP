e5e576dea2cd8a01a30fa0202ddb7633b86122fe
olegnizh <olegnizh@mail.ru>
2024-01-14 18:08:44 +0300

    6.3.2 logic OOP 2

---
+++ ConsoleApp632/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace ConsoleApp632
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Bus bus = new Bus();
+            bus.PrintStatus();
+
+        }
+
+
+
+    }
+
+    class Bus
+    {
+        public int? Load;
+
+        public void PrintStatus()
+        {
+
+            if ( (Load.HasValue) && (Load > 0) )
+            {
+                Console.WriteLine("В авбтобусе {0} пассажиров", Load.Value);
+            }
+            else
+            {
+                Console.WriteLine("Автобус пуст!");
+            }
+
+        }
+    }
+
+}
