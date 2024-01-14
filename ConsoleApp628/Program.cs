9e5bef1ef0f5f0babab08afed087e81d651e8521
olegnizh <olegnizh@mail.ru>
2024-01-14 17:40:15 +0300

    Add project files.

---
+++ ConsoleApp628/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+
+namespace ConsoleApp628
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Rectangle rectangle = new Rectangle();
+
+            int square = rectangle.Square();
+
+            Console.WriteLine("Площадь = {0}", square);
+
+        }
+
+
+
+
+    }
+
+    class Rectangle
+    {
+        public int a = 0;
+        public int b = 0;
+
+        public Rectangle()
+        {
+            a = 6;
+            b = 4;
+
+        }
+
+        public Rectangle(int s)
+        {
+            a = s;
+            b = s;
+
+        }
+
+        public Rectangle(int s1, int s2)
+        {
+            a = s1;
+            b = s2;
+
+        }
+
+        public int Square()
+        {
+            return a * b;
+        }
+
+
+
+
+    }
+
+
+}
+
