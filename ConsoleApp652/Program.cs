658c400a767f06ecd7b429df02581b7ab2df04d0
olegnizh <olegnizh@mail.ru>
2024-01-14 20:36:11 +0300

    6.5.2 begin

---
+++ ConsoleApp652/Program.cs
@@ -0,0 +1,84 @@
+﻿using System;
+
+namespace ConsoleApp652
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Hello World!");
+
+        }
+
+    }
+
+    class CircleObject
+    {
+
+        public double Radius;
+        public double Pi = Math.PI;
+
+        public double Square()
+        {
+            double ret = 0;
+
+            return ret;
+        }
+
+        public double Perimeter()
+        {
+            double ret = 0;
+
+            return ret;
+        }
+
+    }
+    class SquareObject
+    {
+
+        public double Side;
+
+        public double Square()
+        {
+            double ret = 0;
+
+            return ret;
+        }
+
+        public double Perimeter()
+        {
+            double ret = 0;
+
+            return ret;
+        }
+
+    }
+
+
+    class TriangleObject
+    {
+        public double A; // основание
+        public double H; // высота
+        public double Side1;
+        public double Side2;
+        public double Side3;
+
+        public double Square()
+        {
+            double ret = 0;
+
+            return ret;
+        }
+
+        public double Perimeter()
+        {
+            double ret = 0;
+
+            return ret;
+        }
+
+    }
+
+
+}
