64937f05eb72afecccff3c029493423267ebd9de
olegnizh <olegnizh@mail.ru>
2024-01-17 05:35:28 +0300

    6.6.1

---
+++ ConsoleApp661/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace ConsoleApp661
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+
+    class TrafficLight
+    {
+        string color = "";
+
+        private void ChangeColor(string color)
+        {
+
+        }
+
+        public string GetColor()
+        {
+            return color;
+        }
+
+    }
+
+}
