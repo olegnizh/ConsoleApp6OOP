dd744298ce4a4ab991adba814c187010dba0b52a
olegnizh <olegnizh@mail.ru>
2024-01-19 20:26:54 +0300

    7.1.4 Employee наследники

---
+++ ConsoleApp714/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace ConsoleApp714
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+
+        class Employee
+        {
+            public string Name;
+            public int Age;
+            public int Salary;
+        }
+
+        class ProjectManager : Employee
+        {
+            public string ProjectName;
+
+        }
+
+        class Developer : Employee
+        {
+            private string ProgrammingLanguage;
+
+        }
+
+
+
+    }
+}
