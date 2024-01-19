3f965167b75eead28c3df5dedd500bad2b83879f
olegnizh <olegnizh@mail.ru>
2024-01-19 20:30:26 +0300

    7.1.5 ООП плод

---
+++ ConsoleApp715/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+
+namespace ConsoleApp715
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+
+        }
+
+        class Plod
+        {
+
+        }
+
+        class Fruit : Plod
+        {
+
+        }
+
+        class PlodTerra : Plod
+        {
+
+        }
+
+        class Apple : Fruit
+        {
+
+        }
+
+        class Banana : Fruit
+        {
+
+        }
+
+        class Pear : Fruit
+        {
+
+        }
+
+        class Potato : PlodTerra
+        {
+
+        }
+
+        class Carrot : PlodTerra
+        {
+
+        }
+
+
+
+    }
+}
