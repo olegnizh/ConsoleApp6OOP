bd9c858c1df68ea39d49223c13f093d2861c82b0
olegnizh <olegnizh@mail.ru>
2024-01-23 05:02:04 +0300

    7.1.7 name

---
+++ ConsoleApp717/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace ConsoleApp717
+{
+    class Program
+    {
+
+        class SmartHelper
+        {
+            private string name;
+
+            public SmartHelper(string name)
+            {
+                this.name = name;
+            }
+
+            public void Greetings(string name)
+            {
+                //
+                Console.WriteLine("Привет, {0}, я интеллектуальный помощник {1}", name, this.name);
+            }
+        }
+
+
+        static void Main(string[] args)
+        {
+
+            SmartHelper helper = new SmartHelper("Олег");
+            helper.Greetings("Грег");
+
+            Console.ReadKey();
+
+        }
+
+    }
+
+}
